9666f1b610d35dfc80e03bc55770fea614174bd4
whereasme <whereasme@7af3ea8d-c528-7662-7ec3-3cc9a8268a44>
2013-10-10 18:25:20 +0000

    with -m option added for global seq alignment

--- src/sub.sh
+++ src/sub.sh
@@ -1,11 +1,10 @@
 #Note: the #PBS designator is "magic"; it is NOT a comment!
 # set the requested resources
-#PBS -l mem=12000mb,nodes=1:ppn=14,walltime=00:20:00
+#PBS -l mem=150000mb,nodes=2:ppn=64,walltime=23:20:00
 #PBS -V
-
 
 
 cd $PBS_O_WORKDIR
 
-mpirun /home/cwu2/pclust.SR/pclust -f /home/cwu2/pclust/data/20k -s /home/cwu2/pclust/pf_20k.fa.prep.nr -n 20000 -c /home/cwu2/pclust.SR/cfg.conf -g 14 -p 4 -o /home/cwu2/pclust/todel/ > tmp.log
+mpirun /home/cwu2/psgraph/src/pclust -f /home/cwu2/psgraph/src/suffixtree/ -s /mnt/pvfs2/cwu2/bmcrun/data/input.seqs/pf_2M.fa -n 2004241 -m 1 -c /home/cwu2/pclust.SR/cfg.conf -g 64 -p 16 -o /mnt/pvfs2/cwu2/hicomb/ > /mnt/pvfs2/cwu2/hicomb/tmp.log
 
